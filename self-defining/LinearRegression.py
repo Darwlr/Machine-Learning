fe703947f522ca2f34eb93ebbb44338a07b9abc2
wlr <wlr@wlrgit.com>
2020-10-14 15:55:33 +0800

    自定义函数或算法

--- self-defining/LinearRegression.py
+++ self-defining/LinearRegression.py
@@ -18,6 +18,40 @@ class LinearRegression:
         self.coef_ = self._theta[1:]            #系数
 
         return self
+    def fit_gd(self, X_train, y_train, eta=0.01, n_iters=1e4):
+        """根据训练数据集X_train，y_train，使用梯度下降法训练Linear Regression模型"""
+        assert X_train.shape[0] == y_train.shape[0], \
+            "the size of X_train, y_train must be equal to the size of y_train"
+        def J(theta, X_b, y):
+            try:
+                return np.sum((y - X_b.dot(theta)) ** 2) / len(X_b)
+            except:
+                return float('inf')
+        def dJ(theta, X_b, y):
+            res = np.empty(len(theta))
+            res[0] = np.sum(X_b.dot(theta) - y)
+            for i in range(1, len(theta)):
+                res[i] = np.sum((X_b.dot(theta) - y).dot(X_b[:, i]))
+            return res * 2 / len(X_b)
+        def gradient_descent(X_b, y, initial_theta, eta, n_iters=1e4, epsilon=1e-8):
+            theta = initial_theta
+            i_iter = 0
+            while i_iter < n_iters:
+                gradient = dJ(theta, X_b, y)
+                last_theta = theta
+                theta = theta - eta * gradient
+
+                if(abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon):
+                    break
+                i_iter += 1
+            return theta
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.zeros(X_b.shape[1])
+        self._theta = gradient_descent(X_b, y_train, initial_theta, eta)
+
+        self.interception_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+        return self
     def predict(self, X_predict):
         """给定待预测数据集X_predict，返回表示X_predict的结果向量"""
         assert self.interception_ is not None and self.coef_ is not None, \
