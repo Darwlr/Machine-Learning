965ddbe1002d98a4868b7b9c2197c1edec1b1a23
wlr <wlr@wlrgit.com>
2020-10-21 15:42:06 +0800

    self-defining

--- self-defining/metrics.py
+++ self-defining/metrics.py
@@ -30,3 +30,62 @@ def mean_absolute_error(y_true, y_predict):
 def r2_score(y_true, y_predict):
     """计算y_true和y_predict之间的R Square"""
     return 1 - mean_squared_error(y_true, y_predict) / np.var(y_true)
+
+def TN(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 0) & (y_predict == 0))
+def FP(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 0) & (y_predict == 1))
+def FN(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 1) & (y_predict == 0))
+def TP(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 1) & (y_predict == 1))
+# 混淆矩阵
+def confusion_matrix(y_true, y_predict):
+    return  np.array([
+        [TN(y_true, y_predict), FP(y_true, y_predict)],
+        [FN(y_true, y_predict), TP(y_true, y_predict)]
+    ])
+# 精准率
+def precision_score(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    tp = TP(y_true, y_predict)
+    fp = FP(y_true, y_predict)
+    try:
+        return tp / (tp + fp)
+    except:
+        return 0.0
+# 召回率
+def recall_score(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    tp = TP(y_true, y_predict)
+    fn = FN(y_true, y_predict)
+    try:
+        return tp / (tp + fn)
+    except:
+        return 0.0
+# f1_score
+def f1_score(y_true, y_predict):
+    precision = precision_score(y_true, y_predict)
+    recall = recall_score(y_true, y_predict)
+    try:
+        return 2. * precision * recall / (precision + recall)
+    except:
+        return 0.0
+def TPR(y_true, y_predict):
+    tp = TP(y_true, y_predict)
+    fn = FN(y_true, y_predict)
+    try:
+        return tp / (tp + fn)
+    except:
+        return  0.0
+def FPR(y_true, y_predict):
+    tn = TN(y_true, y_predict)
+    fp = FP(y_true, y_predict)
+    try:
+        return fp / (tn + fp)
+    except:
+        return  0.0
